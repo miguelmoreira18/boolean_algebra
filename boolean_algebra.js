4be2b68246476f018a082590665bf6c196bbec87
Khiuta <minhaepic1551@gmail.com>
2024-09-19 20:54:39 -0300

    solving aa = a, a+ab = a, a+~ab = a+b

--- boolean_algebra.js
+++ boolean_algebra.js
@@ -21,18 +21,18 @@ function cleanVariables(variables){
   for(v in variables){
     for(i in variables){
       if(variables[i] === variables[v] && v != i){
-        variables[i] = ''
+        variables.splice(i, 1);
       }
     }
   }
 
-  return variables.join('').split('');
+  return variables;
 }
 
 function solveDistribution(distributions){
   const distributionsDone = [];
   for(dist of distributions){
-    let terms = dist.match(/[a-zA-Z]+/g);
+    let terms = dist.match(/(~?[a-zA-Z])+/g);
     let distributed = '';
     for(let i = 1; i < terms.length; i++){
       distributed += terms[0] + terms[i] + '+';
@@ -49,12 +49,13 @@ function solveDistribution(distributions){
 
 function findVariablesAndDistributions(input){
   input = input.split(' ').join(''); // cleaning whitespaces
-  let variables = input.match(/[a-zA-Z]/g);
+  let variables = input.match(/~?[a-zA-Z]/g);
   variables = cleanVariables(variables);
   console.log(variables);
-  const distributions = input.match(/[a-zA-Z]+\([a-zA-Z]+\+[a-zA-z]+\)/g);
+  const distributions = input.match(/(~?[a-zA-Z])+\((~?[a-zA-Z])+(\+(~?[a-zA-Z])+)*\)/g);
 
-  let finalInput = input.replace(/[a-zA-Z]+\([a-zA-Z]+\+[a-zA-z]+\)/g, '');
+  let finalInput = input.replace(/(~?[a-zA-Z])+\((~?[a-zA-Z])+(\+(~?[a-zA-Z])+)*\)/g, '');
+  
 
   // if(finalInput.split('')[finalInput.length-1] === '+'){
   //   finalInput = finalInput.split('');
@@ -62,32 +63,83 @@ function findVariablesAndDistributions(input){
   //   finalInput = finalInput.join('');
   // }
 
-  const solvedDistributions = solveDistribution(distributions);
+  //#region taking off repeated + signs
+  finalInput = finalInput.split('');
+  for(let i = 1; i < finalInput.length; i++){
+    if(finalInput[i] == finalInput[i-1]){
+      finalInput.splice(i, 1);
+    }
+  }
+  finalInput.push('+');
+  finalInput = finalInput.join('');
+  console.log('final: ' + finalInput)
+  //#endregion
+  if(distributions != null){
+    const solvedDistributions = solveDistribution(distributions);
 
-  for(dist of solvedDistributions){
-    finalInput += dist;
+    for(dist of solvedDistributions){
+      finalInput += dist;
+    }
   }
 
   console.log(finalInput);
-  findAllTerms(finalInput);
+  solveAuxiliarIdentities(finalInput);
 }
 
-function findAllTerms(input){
-  const terms = input.match(/[a-zA-Z]+/g);
+function solveAuxiliarIdentities(input){
+  const terms = input.match(/(~?[a-zA-Z])+/g);
   console.log(terms);
+
+  //A + ~AB = A + B
+  for(term in terms){
+    for(j in terms){
+      const re = new RegExp(`~${terms[term]}`, 'g');
+      if(terms[j].match(re) && term != j){
+        terms[j] = terms[j].replace(re, '');
+      }
+    }
+  }
+  
+  //~A + AB = ~A + B
 
   //A + AB = A
   for(term in terms){
     for(j in terms){
       const re = new RegExp(`${terms[term]}`, 'g');
       if(terms[j].match(re) && term != j){
-        console.log(j);
         terms.splice(j, 1);
       }
     }
   }
-  console.log(terms)
 
+  //AA = A
+  for(t in terms){
+    terms[t] = cleanVariables(terms[t].split('')).join('');
+  }
 
-  //A + ~AB = A + B
+  //A+A = A
+
+  //A+~A = 1
+
+  //A~A = 0
+
+  //A+1 = 1
+
+  //A+0 = A
+
+  //A1 = 1
+
+  //A0 = 0
+
+  let finalAnswer = ''
+
+  for(term in terms){
+    if(term < terms.length-1){
+      finalAnswer += terms[term] + '+'
+    } else {
+      finalAnswer += terms[term]
+    }
+  }
+
+  console.log(finalAnswer);
 }
